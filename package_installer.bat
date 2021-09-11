71c81a48b1dceb55567cb675c8693778a5e57312
Sagar Kumar <74974420+sagarkumar3105@users.noreply.github.com>
2021-09-11 12:20:02 +0530

    Add files via upload

---
+++ package_installer.bat
@@ -0,0 +1,11 @@
+@echo off
+set /p loc=Enter your location c:/.../python39/scripts:
+pip install numpy
+pip install pandas
+pip install pillow
+pip install scikit-learn
+pip install scikit-image
+pip install yfinance
+pip install jupyterlab
+echo ********************Setup completed*******************
+pause
