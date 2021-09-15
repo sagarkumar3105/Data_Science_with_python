eeb1da8213fc98873f230b2bbb6d5e11ad8e4d4e
Sagar Kumar <74974420+sagarkumar3105@users.noreply.github.com>
2021-09-15 22:58:41 +0530

    Update package_installer.bat

--- package_installer.bat
+++ package_installer.bat
@@ -3,6 +3,7 @@ set /p loc=Enter your location c:/.../python39/scripts:
 pip install numpy
 pip install pandas
 pip install pillow
+pip install seaborn
 pip install scikit-learn
 pip install scikit-image
 pip install yfinance
